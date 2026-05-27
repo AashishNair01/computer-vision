7ee9ae360e9e14fd029810e6d5a913f050a20f6e
Mark <markstephenthomas10@gmail.com>
2026-05-27 21:44:23 +0200

    Added augmentations for Q3

--- Assignment_3/src/utils/transform_presets.py
+++ Assignment_3/src/utils/transform_presets.py
@@ -19,14 +19,40 @@ presets = dict(
     # Note that you usually need the augmentation **only** for training time!
     # E.g. CIFAR10_WithFlip=dict()
     ##############
+    # Augmentation 1 (Strictly Geometric / Spatial)
     CIFAR10_WithFlip=dict(
         train=transforms.Compose([
-            # Geometric Augmentations
+            transforms.RandomHorizontalFlip(p=0.5),
+            transforms.RandomCrop(32, padding=4, padding_mode='reflect'),
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+        ]),
+        eval=transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        ])
+    ),
+
+    # Augmentation 2 (Strictly Color-Space Adjustments)
+    CIFAR10_ColorJitter=dict(
+        train=transforms.Compose([
+            transforms.ColorJitter(brightness=0.3, contrast=0.3, saturation=0.3, hue=0.1),
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+        ]),
+        eval=transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        ])
+    ),
+
+    # Augmentation 3 (Heavy Combined Paradigm)
+    CIFAR10_HeavyAug=dict(
+        train=transforms.Compose([
             transforms.RandomHorizontalFlip(p=0.5),
             transforms.RandomCrop(32, padding=4, padding_mode='reflect'),
             transforms.RandomRotation(degrees=15),
-            # Color Space Augmentations
-            transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),          
+            transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
             transforms.ToTensor(),
             transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
         ]),
